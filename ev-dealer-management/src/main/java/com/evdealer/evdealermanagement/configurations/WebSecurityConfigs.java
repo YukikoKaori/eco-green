e79a95272136ea43f0b8efae0a6adca60f42be1b
NhanHuynhThien <nhanhuynh7115@gmail.com>
2025-09-18 02:20:45 +0700

    Upload Code WebSecurityConfigs

--- ev-dealer-management/src/main/java/com/evdealer/evdealermanagement/configurations/WebSecurityConfigs.java
+++ ev-dealer-management/src/main/java/com/evdealer/evdealermanagement/configurations/WebSecurityConfigs.java
@@ -1,7 +1,63 @@
 package com.evdealer.evdealermanagement.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfigs {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:5173/"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setExposedHeaders(List.of("Authorization"));
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain publicFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer:: disable)
+                .securityMatcher("/auth/", "/vehicle/**", "/battery/**")
+                .cors(cors -> cors.configurationSource(configurationSource()))
+                .authorizeHttpRequests((auth -> auth.anyRequest().permitAll()));
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain privateFilterChain (HttpSecurity http) throws Exception{
+        http
+                .securityMatcher("/member/**", "/admin/**")
+                .csrf(AbstractHttpConfigurer:: disable)
+                .cors(cors -> cors.configurationSource(configurationSource()))
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
