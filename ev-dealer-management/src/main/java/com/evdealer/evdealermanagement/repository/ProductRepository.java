99b1b720b7b54cd04a7b42bd7139cd26e5958133
NhanHuynhThien <nhanhuynh7115@gmail.com>
2025-09-20 08:05:08 +0700

    Config WebSecurityConfig and Add code show Product List

--- ev-dealer-management/src/main/java/com/evdealer/evdealermanagement/repository/ProductRepository.java
+++ ev-dealer-management/src/main/java/com/evdealer/evdealermanagement/repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.evdealer.evdealermanagement.repository;
 
+import com.evdealer.evdealermanagement.dto.product.detail.ProductDetail;
 import com.evdealer.evdealermanagement.entity.product.Products;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface ProductRepository {
-
-    List<Products> getAllProducts();
+@Repository
+public interface ProductRepository extends JpaRepository<Products, Long> {
 }
