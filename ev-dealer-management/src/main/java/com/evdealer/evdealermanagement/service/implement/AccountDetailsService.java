d8b2de92ccb63d5d0d0f802ca2fbfac5d3ec0d4f
NhanHuynhThien <nhanhuynh7115@gmail.com>
2025-09-27 10:22:29 +0700

    Recode Login API By Using Phonenumber

--- ev-dealer-management/src/main/java/com/evdealer/evdealermanagement/service/implement/AccountDetailsService.java
+++ ev-dealer-management/src/main/java/com/evdealer/evdealermanagement/service/implement/AccountDetailsService.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class AccountDetailsService implements UserDetailsService {
 
@@ -21,12 +23,18 @@ public class AccountDetailsService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Account account = accountRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    public UserDetails loadUserByUsername(String usernameOrPhone) throws UsernameNotFoundException {
+        Optional<Account> accountOpt;
+        if (usernameOrPhone.matches("\\d+")) { // nếu chỉ là số → phone
+            accountOpt = accountRepository.findByPhone(usernameOrPhone);
+        } else {
+            accountOpt = accountRepository.findByUsername(usernameOrPhone);
+        }
 
+        Account account = accountOpt.orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
         return new CustomAccountDetails(account);
     }
+
 
     public UserDetails loadUserByPhone(String phone) throws UsernameNotFoundException {
         Account account = accountRepository.findByPhone(phone)
